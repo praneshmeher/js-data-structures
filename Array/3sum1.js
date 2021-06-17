c43a71f1bc7ff963aec5237aca16d425b6780020
Pranesh <praneshmeher@gmail.com>
2021-06-17 14:36:13 +0530

    commit

--- Array/3sum1.js
+++ Array/3sum1.js
@@ -4,18 +4,31 @@
 // whose sum is equal to the given value. If there is such a triplet present in array, 
 // then print the triplet and return true. Else return false.
 
+// function find3sum(arr, key){
+//     for (let i = 0; i < arr.length-1; i++) {
+//         let map = {}
+//         for (let j = i+1; j < arr.length; j++) {
+//             let sum = (arr[i]+arr[j])
+//             if(map[key-arr[j]])
+//                 console.log('ans---', arr[i], arr[j], map[key-arr[j]])
+//             else
+//                 map[sum]=arr[j]
+//         }
+//     }
+//     return false
+// }
+
 function find3sum(arr, key){
     for (let i = 0; i < arr.length-1; i++) {
         let map = {}
         for (let j = i+1; j < arr.length; j++) {
-            let sum = (arr[i]+arr[j])
-            if(map[key-arr[j]])
-                console.log('ans---', arr[i], arr[j], map[key-arr[j]])
+            let sum = arr[i] + arr[j]
+            if(map[key-sum])
+                console.log('====', arr[i], arr[j], map[key-sum])
             else
-                map[sum]=arr[j]
+                map[arr[j]]=arr[j]
         }
     }
-    return false
 }
 
 let arr = [1, 4, 45, 6, 10, 8];
