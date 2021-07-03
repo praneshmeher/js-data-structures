317c171f9c91ea8d4bc9929595ad6a81d5450091
Pranesh <praneshmeher@gmail.com>
2021-07-03 14:34:04 +0530

    commit

--- Array/rotation.js
+++ Array/rotation.js
@@ -13,5 +13,16 @@ function rotation(arr, n){
     return arr
 }
 
+function rotation1(arr, n){
+    for (let i = 0; i < n; i++) {
+        let temp = arr[arr.length-1]
+        for (let j = arr.length-1; j > 0; j--) {
+            arr[j]=arr[j-1]
+        }
+        arr[0]=temp
+    }
+    return arr
+}
+
 let arr = [1,2,3,4,5]
-console.log(rotation(arr,2))
+console.log(rotation1(arr,2))
