725ad820cf0fe6c261d0842963c77bd8f11fee03
Pranesh <praneshmeher@gmail.com>
2021-07-03 17:42:37 +0530

    commit

--- Strings/only-digits.js
+++ Strings/only-digits.js
@@ -4,7 +4,7 @@ function onlyDigits(){
     let ans = false
 
     for (let i = 0; i < str.length; i++) {
-        if(str.charAt(i) >= 0 && str.charAt(i) <= 9){
+        if(str[i] >= 0 && str[i] <= 9){
             ans = true
         }else{
             ans= false
