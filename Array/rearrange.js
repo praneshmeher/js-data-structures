03dc33aafb87858745328c603be1e1964261dc44
Pranesh <praneshmeher@gmail.com>
2021-07-03 14:45:20 +0530

    commit

--- Array/rearrange.js
+++ Array/rearrange.js
@@ -14,8 +14,35 @@ function rearrange(arr){
         if(arr[i]!==i)
         arr[i]=-1
     }
+    return arr
 }
 
+function rearrange1(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] >= 0 && i != arr[i]){
+            let temp = arr[arr[i]]
+            arr[arr[i]]=arr[i]
+            arr[i]=temp
+        }
+    }
+    return arr
+}
+
+function rearrange(arr){
+    let map = {}
+    for (let i = 0; i < arr.length; i++) {
+        map[arr[i]]=1
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if(map[i]){
+            arr[i] = i
+        }else{
+            arr[i] = -1
+        }
+    }
+    return arr
+}
+
+
 let arr = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
-rearrange(arr)
-console.log(arr)
+console.log(rearrange(arr))
