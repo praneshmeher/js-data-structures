ad3950a36ef159262da96425ff8a0ee5893a3f07
Pranesh <praneshmeher@gmail.com>
2021-07-03 14:42:47 +0530

    commit

--- Array/wave.js
+++ Array/wave.js
@@ -10,5 +10,37 @@ function wave(arr){
     return arr
 }
 
+function wave1(arr){
+    for (let i = 0; i < arr.length; i+=2) {
+        if(i<arr.length-1 && arr[i]<arr[i+1]){
+            let temp = arr[i]
+            arr[i]=arr[i+1]
+            arr[i+1]=temp
+        }
+        if(i>0 && arr[i]<arr[i-1]){
+            let temp = arr[i]
+            arr[i]=arr[i-1]
+            arr[i-1]=temp
+        }
+    }
+    return arr
+}
+
+function wave2(arr){
+    for (let i = 0; i < arr.length; i+=2) {
+        if(i < arr.length-1 && arr[i]>arr[i+1]){
+            let temp = arr[i]
+            arr[i]=arr[i+1]
+            arr[i+1]=temp
+        }
+        if(i>0 && arr[i]>arr[i-1]){
+            let temp = arr[i]
+            arr[i]=arr[i-1]
+            arr[i-1]=temp
+        }
+    }
+    return arr
+}
+
 let arr = [ 10, 90, 49, 2, 1, 5, 23 ];
-console.log(wave(arr))
+console.log(wave1(arr))
