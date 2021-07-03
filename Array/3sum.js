15c7e37e598daa507d8416f22bf92ab4c70e16e4
Pranesh <praneshmeher@gmail.com>
2021-07-03 14:56:24 +0530

    commit

--- Array/3sum.js
+++ Array/3sum.js
@@ -1,19 +1,5 @@
 // Find all triplets with zero sum
-
 // Given an array of distinct elements. The task is to find triplets in the array whose sum is zero.
-
-// function find3sum(arr){
-//     for (let i = 0; i < arr.length-1; i++) {
-//         let map = {}
-//         for (let j = i+1; j < arr.length; j++) {
-//             let nsum = -(arr[i]+arr[j])
-//             if(map[nsum])
-//                 console.log('ans----', nsum, arr[i], arr[j])
-//             else
-//                 map[arr[j]]=arr[j]
-//         }
-//     }
-// }
 
 function find3sum(arr){
     for (let i = 0; i < arr.length-1; i++) {
@@ -21,7 +7,7 @@ function find3sum(arr){
         for (let j = i+1; j < arr.length; j++) {
             let sum = -(arr[i]+arr[j])
             if(map[sum])
-                console.log('====', arr[i], arr[j], map[sum])
+                console.log('===', arr[i], arr[j], map[sum])
             else
                 map[arr[j]]=arr[j]
         }
@@ -30,3 +16,20 @@ function find3sum(arr){
 
 let arr = [0, -1, 2, -3, 1];
 find3sum(arr)
+
+function find3sum1(arr, key){
+    for (let i = 0; i < arr.length-1; i++) {
+        let map = {}
+        for (let j = i+1; j < arr.length; j++) {
+            let sum = arr[i] + arr[j]
+            if(map[key-sum])
+                console.log('===', arr[i], arr[j], map[key-sum])
+            else
+                map[arr[j]]=arr[j]
+        }
+    }
+    return false
+}
+
+let arr1 = [1, 4, 45, 6, 10, 8];
+find3sum1(arr1, 22)
