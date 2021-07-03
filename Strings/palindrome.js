6ce37d959185baa278a5eb1fd1378b431d8ef4bb
Pranesh <praneshmeher@gmail.com>
2021-07-03 17:55:03 +0530

    commit

---
+++ Strings/palindrome.js
@@ -0,0 +1,11 @@
+
+function palindrome(str){
+    for (let i = 0; i < str.length/2; i++) {
+        if(str[i] !== str[str.length-1-i])
+            return false
+    }
+    return true
+}
+
+let str = 'madam'
+console.log(palindrome(str))
