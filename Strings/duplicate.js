57f5e3b52fbc5b9660cfbcffcfcae1738b7689ab
Pranesh <praneshmeher@gmail.com>
2021-07-03 18:56:09 +0530

    commit

--- Strings/duplicate.js
+++ Strings/duplicate.js
@@ -11,14 +11,24 @@ function duplicate(str){
     return map
 }
 
-
+function duplicate1(str){
+    let map = {}
+    let ans = ''
+    for (let i = 0; i < str.length; i++) {
+        if(!map[str[i]]){
+            map[str[i]]=1
+            ans=ans+str[i]
+        }
+    }
+    console.log('removed duplicates', ans)
+}
 
 let str = 'abrakadabra'
 let map = duplicate(str)
 
 for(let key in map){
     if(map[key] == Math.max(...Object.values(map)))
-        console.log(key)
+        console.log('maximum occurring character', key)
 }
 
 
